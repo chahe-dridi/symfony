270620d5f56a055dade69e404e255dc6b7af4f67
chaher dridi <chaher.dridi.100@gmail.com>
2023-09-25 21:54:20 +0100

    first push

---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ServiceController extends AbstractController
+{
+    #[Route('/service', home: 'app_service')]
+    public function index(): Response
+    {
+        return $this->render('service/index.html.twig', [
+            'controller_name' => 'ServiceController',
+        ]);
+    }
+}
